4debbd0423b50c5a15e24360ba22803da873aa64
Shoaib <shoaib.ashfaq@mslm.io>
2025-03-19 16:38:27 +0500

    greeting program

---
+++ assignments/greeting.py
@@ -0,0 +1,9 @@
+print("Welcome to the Greeting Program!")
+
+name = input("What is your name? ")
+
+favorite_food = input("What is your favorite food? ")
+
+greeting = "Hello, " + name.title() + "! It's awesome that you love " + favorite_food + "."
+
+print(greeting)
